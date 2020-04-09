de96b602d60970a90e970a0c80bb968f27b6b4ec
wissal <wissal.ouarsafi@gmail.com>
2020-04-09 13:40:02 -0700

    Display the data in the UI, Create pagination

--- script.js
+++ script.js
@@ -9,8 +9,39 @@ async function searchSongs(term) {
     const res = await fetch(`${apiURL}/suggest/${term}`);
     const data = await res.json();
   
-    console.log(data);
+    showData(data);
 }
+
+async function getMoreSongs(url) {
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await res.json();
+  
+    showData(data);
+}
+
+function showData(data) {
+    result.innerHTML = `
+      <ul class="songs">
+        ${data.data
+          .map(
+            song => `<li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      </li>`
+          )
+          .join('')}
+      </ul>
+    `;
+
+    if(data.prev || data.next) {
+        more.innerHTML = `
+            ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
+            ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+        `;
+    } else {
+        more.innerHTML = ``;
+    }
+} 
 
 form.addEventListener('submit', e =>{
     e.preventDefault();
