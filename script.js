72d5d7b3a5abb24610cb6b55f509aaa4a0771a1c
wissal <wissal.ouarsafi@gmail.com>
2020-04-09 14:19:24 -0700

    Create the functionality needed to get lyrics of each song

--- script.js
+++ script.js
@@ -43,6 +43,25 @@ function showData(data) {
     }
 } 
 
+async function getLyrics(artist, song) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${song}`);
+    const data = await res.json();
+
+    if (data.error) {
+        result.innerHTML = data.error;
+   } else {
+        const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+        result.innerHTML = `
+            <h2><strong>${artist}</strong> - ${song}</h2>
+            <span>${lyrics}</span>
+        `;
+  }
+
+  more.innerHTML = '';
+
+}
+
 form.addEventListener('submit', e =>{
     e.preventDefault();
 
@@ -54,3 +73,16 @@ form.addEventListener('submit', e =>{
         searchSongs(searchTerm);
     }
 });
+
+
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+
+    if(clickedEl.tagName === 'BUTTON'){
+        const artist = clickedEl.getAttribute('data-artist');
+        const song = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist, song);
+    }
+
+})
